ac2df5f742ef43b1367d1e68bf7b88b59e4f8836
Carlos Carvalho <cccarv82@gmail.com>
2023-12-18 13:39:25 -0300

    removed the robot installation from the beginning bc its already on requirements

--- automation_fw_setup/check_installations.py
+++ automation_fw_setup/check_installations.py
@@ -5,45 +5,30 @@ import inquirer
 from colorama import Fore, Style
 import os
 
-def install_robot_framework(project_name):
-    print("Installing Robot Framework...")
-    python_path = f"{project_name}/venv/bin/python" if os.name != 'nt' else f"{project_name}\\venv\\Scripts\\python"
-    subprocess.run([python_path, "-m", "pip", "install", "robotframework"], check=True)
-
-def check_installation(program_name, check_command, project_name, install_function=None):
+def check_installation(program_name, check_command, project_name):
     if program_name == "Python":
         version = platform.python_version()
         print(f"{Fore.GREEN}✓{Style.RESET_ALL} {program_name} version detected: {version}")
         return True
     else:
         check_command = check_command.split()  # split the command into a list
-        venv_activate = os.path.join(project_name, 'venv', 'Scripts', 'activate') if os.name == 'nt' else f"source {project_name}/venv/bin/activate"
-        check_command = f"{venv_activate} && {' '.join(check_command)}"
+        check_command = ' '.join(check_command)
         result = subprocess.run(check_command, capture_output=True, text=True, shell=True)
         output = result.stdout.strip() if result.stdout.strip() else result.stderr.strip()
         if result.returncode != 0:
-            if install_function:
-                questions = [
-                    inquirer.Confirm('install',
-                                     message=f"Do you want to install {program_name} in the Virtual Env ?"),
-                ]
-                answers = inquirer.prompt(questions)
-                if answers['install']:
-                    install_function(project_name)
-                    return True
             return False
         else:
             print(f"{Fore.GREEN}✓{Style.RESET_ALL} {program_name} version detected: {output}")
             return True
 
 def check_installations(project_name):
-    python_path = f"{project_name}/venv/bin/python" if os.name != 'nt' else f"{project_name}\\venv\\Scripts\\python"
     programs = [
         {"name": "Python", "command": ""},
         {"name": "pip", "command": "pip --version"},
-        {"name": "Robot Framework", "command": f"{python_path} -m robot --version", "install": install_robot_framework},
         {"name": "Node.js", "command": "node --version", "install_url": "https://nodejs.org/en/download/"},
     ]
 
     for program in programs:
-        check_installation(program["name"], program["command"], project_name, program.get("install"))
+        if not check_installation(program["name"], program["command"], project_name):
+            print(f"{Fore.RED}✗{Style.RESET_ALL} {program['name']} not found. Please install it from {program.get('install_url', 'the official website')}.")
+            sys.exit(1)
